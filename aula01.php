b61871012b2219507515c2903d62c29e660a072d
FidelisArtur <arturfidelis@pactosolucoes.com.br>
2022-05-03 13:44:06 -0300

    add

--- aula01.php
+++ aula01.php
@@ -5,6 +5,17 @@ $altura = 1.81;
 $nome = "Artur Fidelis";
 $casado = true;
 
+$numero = 50;
+
+define('CONFIG', 1500);
+
+// echo CONFIG;
+
+function calc(){
+    global $numero;
+    echo $numero;
+}
+
 $cadastro = array("cliente1" => "Artur", "cliente2" => "Ricardo");
 $cadastro1 = array(
     "cliente1" => array(
